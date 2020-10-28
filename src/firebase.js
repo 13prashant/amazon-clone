489197727228098a4806da035dd5b83724f66fe5
Prashant Patel <pacific1392@gmail.com>
2020-10-28 17:44:48 +0530

    first commit-implemented react-router-dom & react-currency-format

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA5C0E1Oq-2E4VxMvY7pZKU4eIms0Dkkso",
+    authDomain: "clone-14a13.firebaseapp.com",
+    databaseURL: "https://clone-14a13.firebaseio.com",
+    projectId: "clone-14a13",
+    storageBucket: "clone-14a13.appspot.com",
+    messagingSenderId: "50090837873",
+    appId: "1:50090837873:web:3ceb6250d8573e578f58d0",
+    measurementId: "G-YL33BC3RLN"
+};
